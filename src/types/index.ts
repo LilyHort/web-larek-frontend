916b3595ecf56c47c6cdc247e3f25ef11be926cf
Лилиана Леднева <lilyledneva@gmail.com>
2024-05-06 00:20:59 +0300

    Обновлет документацию

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,25 +1,34 @@
 // Интерфейс структуры продукта
 export interface IProduct {
     id: string;
-    description?: string;
-    image?: string;
+    description: string;
+    image: string;
     title: string;
-    category?: string;
-    price: number;
+    category: string;
+    price: number | null;
+	button?: HTMLButtonElement;
   }
 
 // Интерфейс списка продуктов
 export interface IProductList {
-  total: number;
+  	total: number;
 	items: IProduct[];
 }
 
-// Интерфейс заказа
-export interface IOrder {
-	email: string;
-	phone: string;
+// Интерфейс формы оплаты
+export interface IPaymentForm {
 	address: string;
 	payment: string;
+}
+
+// Интерфейс формы контактов
+export interface IContactsForm {
+	email: string;
+	phone: string;
+}
+
+// Интерфейс заказа
+export interface IOrder extends IPaymentForm, IContactsForm {
 	total: number;
 	items: string[];
 }
@@ -31,5 +40,22 @@ export interface IOrderResult {
 	error?: string;
 }
 
+// Интерфейс модели приложения
+export interface IAppState {
+	catalog: IProduct[];
+	basket: IProduct[];
+	preview: string | null;
+	order: IOrder | null;
+  }
 
-  
+
+// Интерфейс к главноей странице
+interface IMainPageUI {
+    counter: number;
+    catalog: HTMLElement[];
+}
+
+// Интерфейс успешной покупки
+interface IPurchaseResultUI {
+    total: number;
+}
